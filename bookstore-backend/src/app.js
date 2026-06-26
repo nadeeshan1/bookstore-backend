8dd634b3c63c70ecf1c4c93b0064cd062d3845d3
nadeeshan1 <kavindunadeeshan2002@gmail.com>
2026-06-26 11:50:06 +0530

    feat: setup MongoDB connection and Express server

---
+++ bookstore-backend/src/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(morgan('combined'));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Bookstore API is running",
+  });
+});
+
+export default app;
